9db20cce348e5935e4d50746c24e101ded76dd78
Bob Egan <bob.egan.dev@gmail.com>
2024-07-10 14:46:29 -0400

    Setup for vercel migration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portal/',
+  base: '/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
